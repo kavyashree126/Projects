b112530661d5bbe313523445ee3f78d2bb9c19b1
kavyashree126 <135593816+kavyashree126@users.noreply.github.com>
2023-06-23 22:05:40 +0530

    Add files via upload

---
+++ micro project 3/index.js
@@ -0,0 +1,199 @@
+const arr = 
+[
+  {
+    "id": 1,
+    "foodname": "Apple",
+    "calorie": 95,
+    "category": "Fruit",
+    "protiens": 0.3,
+    "cab": 25
+  },
+  {
+    "id": 2,
+    "foodname": "Banana",
+    "calorie": 105,
+    "category": "Fruit",
+    "protiens": 1.3,
+    "cab": 27
+  },
+  {
+    "id": 3,
+    "foodname": "Orange",
+    "calorie": 62,
+    "category": "Fruit",
+    "protiens": 1.2,
+    "cab": 15
+  },
+  {
+    "id": 4,
+    "foodname": "Pear",
+    "calorie": 95,
+    "category": "Fruit",
+    "protiens": 0.5,
+    "cab": 25
+  },
+  {
+    "id": 5,
+    "foodname": "Grapefruit",
+    "calorie": 42,
+    "category": "Fruit",
+    "protiens": 1.1,
+    "cab": 11
+  },
+  {
+    "id": 6,
+    "foodname": "Strawberry",
+    "calorie": 46,
+    "category": "Fruit",
+    "protiens": 1.5,
+    "cab": 10
+  },
+  {
+    "id": 7,
+    "foodname": "Blueberry",
+    "calorie": 52,
+    "category": "Fruit",
+    "protiens": 1.1,
+    "cab": 14
+  },
+  {
+    "id": 8,
+    "foodname": "Raspberry",
+    "calorie": 52,
+    "category": "Fruit",
+    "protiens": 1.4,
+    "cab": 15
+  },
+  {
+    "id": 9,
+    "foodname": "Broccoli",
+    "calorie": 34,
+    "category": "Vegetable",
+    "protiens": 3.3,
+    "cab": 5
+  },
+  {
+    "id": 10,
+    "foodname": "Cauliflower",
+    "calorie": 25,
+    "category": "Vegetable",
+    "protiens": 2.6,
+    "cab": 5
+  },
+  {
+    "id": 11,
+    "foodname": "Green Beans",
+    "calorie": 31,
+    "category": "Vegetable",
+    "protiens": 2.4,
+    "cab": 4
+  },
+  {
+    "id": 12,
+    "foodname": "Asparagus",
+    "calorie": 20,
+    "category": "Vegetable",
+    "protiens": 2.2,
+    "cab": 2
+  },
+  {
+    "id": 13,
+    "foodname": "Spinach",
+    "calorie": 23,
+    "category": "Vegetable",
+    "protiens": 3.5,
+    "cab": 4
+  },
+  {
+    "id": 14,
+    "foodname": "Kale",
+    "calorie": 33,
+    "category": "Vegetable",
+    "protiens": 4.3,
+    "cab": 7
+  },
+  {
+    "id": 15,
+    "foodname": "Sweet Potato",
+    "calorie": 103,
+    "category": "Vegetable",
+    "protiens": 2,
+    "cab": 27
+  },
+  {
+    "id": 16,
+    "foodname": "Potato",
+    "calorie": 161,
+    "category": "Vegetable",
+    "protiens": 4.3,
+    "cab": 37
+  },
+  {"id": 17,
+    "foodname": "Carrot",
+    "calorie": 41,
+    "category": "Vegetable",
+    "protiens": 0.9,
+    "cab": 9
+  },
+  {
+    "id": 18,
+    "foodname": "Onion",
+    "calorie": 40,
+    "category": "Vegetable",
+    "protiens": 1.4,
+    "cab": 9
+  },
+  {
+    "id": 19,
+    "foodname": "Egg",
+    "calorie": 77,
+    "category": "Protein",
+    "protiens": 6.3,
+    "cab": 0.5
+  },
+  {
+    "id": 20,
+    "foodname": "Chicken Breast",
+    "calorie": 165,
+    "category": "Protein",
+    "protiens": 31,
+    "cab": 0
+  }
+]
+
+
+  function filterArrayByCategory(category) 
+  {
+    return arr.filter(el => el.category === category);
+  }
+  console.log(filterArrayByCategory('Vegetable'));
+
+  function filterArrayByCategory(category) 
+  {
+    return arr.filter(el => el.category === category);
+  }
+  console.log(filterArrayByCategory('Fruit'));
+
+  function filterArrayByCategory(category) 
+  {
+    return arr.filter(el => el.category === category);
+  }
+  console.log(filterArrayByCategory('Protein'));
+
+ 
+  const above100Calories = arr.filter(
+    (arr) => arr.calorie > 100
+  );
+  console.log(above100Calories);
+
+  const below100Calories = arr.filter(
+    (arr) => arr.calorie < 100
+  );
+  console.log(below100Calories);
+
+  const sprotein = arr.sort((a, b) => b.protiens - a.protiens);
+console.log(sprotein);
+
+const scab = arr.sort((a, b) => a.cab - b.cab);
+console.log(scab);
+
