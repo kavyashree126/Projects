046c6cced2ab7a4c9dcfb35da6a539ef5494542f
kavyashree126 <135593816+kavyashree126@users.noreply.github.com>
2023-09-05 23:26:26 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,78 +1,21 @@
-import { useEffect, useReducer, useState } from "react";
-
-import "./App.css";
-
-import backgroundDesktop from "./images/bg-main-desktop.png";
-import backgroundMobile from "./images/bg-main-mobile.png";
-import CardForm from "./components/CardForm/CardForm";
-import Cards from "./components/Cards/Cards";
-import Success from "./components/Success/Success";
-
-const initialCardState = {
-	cardholderName: "",
-	cardNumber: "",
-	expiryDate: {
-		mm: "",
-		yy: "",
-	},
-	cvc: "",
-};
-
-const reducer = (currentCardValue, action) => {
-	switch (action.case) {
-		case "cardholderName":
-			return { ...currentCardValue, cardholderName: action.value };
-		case "cardNumber":
-			return { ...currentCardValue, cardNumber: action.value };
-		case "mm":
-			return {
-				...currentCardValue,
-				expiryDate: { ...currentCardValue.expiryDate, mm: action.value },
-			};
-		case "yy":
-			return {
-				...currentCardValue,
-				expiryDate: { ...currentCardValue.expiryDate, yy: action.value },
-			};
-		case "cvc":
-			return { ...currentCardValue, cvc: action.value };
-		case "reset":
-			return initialCardState;
-		default:
-			return currentCardValue;
-	}
-};
-
+import Select_Music from "./Pages/Select_Music";
+import SignUp from "./Pages/SignUp";
+import Dashboard from "./Pages/Dashboard";
+import Home_Page from "./Pages/Home_Page";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-	const [cardState, cardDispatch] = useReducer(reducer, initialCardState);
-	const [mobile, setMobile] = useState(window.matchMedia("(max-width:700px)").matches);
-	const [formIsValid, setFormIsValid] = useState(false);
-
-	function validateForm() {
-		setFormIsValid(true);
-	}
-	function clearForm() {
-		cardDispatch({ case: "reset" });
-	}
-	useEffect(() => {
-		window.matchMedia("(max-width:700px)").addEventListener("change", (e) => setMobile(e.matches));
-	}, []);
-
-	const styles = {
-		backgroundImage: !mobile ? `url(${backgroundDesktop})` : `url(${backgroundMobile})`,
-		backgroundRepeat: "no-repeat",
-	};
-
-	return (
-		<main style={styles}>
-			<Cards cardState={cardState} />
-			{formIsValid ? (
-				<Success clearForm={clearForm} setFormIsValid={setFormIsValid} />
-			) : (
-				<CardForm validateForm={validateForm} cardState={cardState} cardDispatch={cardDispatch} />
-			)}
-		</main>
-	);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+          <Route path="categories" element={<Select_Music />} />
+          <Route path="Dashboard" element={<Dashboard />} />
+          <Route path="Home" element={<Home_Page />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
